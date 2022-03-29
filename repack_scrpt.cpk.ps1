7144b8eb22f7dd70ed7df04b7a2bbe95ccd2a054
TheSola10 <me@thesola.io>
2022-03-29 10:36:06 +0200

    Working version of repack_scrpt.cpk 🎉

--- repack_scrpt.cpk.ps1
+++ repack_scrpt.cpk.ps1
@@ -3,15 +3,15 @@ md -p   .\out\contents\0100D12014FC200\romfs\Data\StreamingAssets
 
 Get-ChildItem -Path .\patches\Data\StreamingAssets\scrpt.cpk -Filter *.json |
 Foreach-Object {
-    Write-Output "Encoding"+$_.Name
+    Write-Output ("Encoding "+ $_.BaseName + ".json")
     python3 .\inucode.py $_.FullName > $(Join-Path .\work\Data\StreamingAssets\scrpt.cpk.contents $_.BaseName)
 }
 
 .\3rdparty\cpkmakec.exe `
     .\work\Data\StreamingAssets\scrpt.cpk.contents `
     .\out\contents\0100D12014FC200\romfs\Data\StreamingAssets\scrpt.cpk `
-    -mode=FILENAME
-    -code=UTF-8
+    -mode=FILENAME  `
+    -code=UTF-8     `
     -align=2048
 
 Remove-Item -r .\work\Data\StreamingAssets\scrpt.cpk.contents
