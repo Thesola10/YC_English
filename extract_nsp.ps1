fb73f49541f0e7550a986baf14ecbe76131ac5d8
TheSola10 <me@thesola.io>
2022-03-29 10:07:29 +0200

    Fixed positional argument in  pwsh

--- extract_nsp.ps1
+++ extract_nsp.ps1
@@ -4,7 +4,7 @@
 
 md .\work
 
-.\3rdparty\hactool.exe -x $1 --pfs0dir=.\work -t pfs0
+.\3rdparty\hactool.exe -x $args[0] --pfs0dir=.\work -t pfs0
 .\3rdparty\hactool.exe -x .\work\97105c79474118d294c8808a22f39bd3.nca `
     --titlekey=$(Format-Hex -Path .\work\0100d12014fc2000000000000000000b.tik -Count 16 -Offset 0x180) `
     --romfsdir=.\work
