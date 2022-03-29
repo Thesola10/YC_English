aba0f7bef36e850c5452d11fa24a8acefce639e8
TheSola10 <me@thesola.io>
2022-03-29 10:05:24 +0200

    Fixing format-hex syntax

--- extract_nsp.ps1
+++ extract_nsp.ps1
@@ -2,9 +2,11 @@
 # This script automatically pulls the titlekey from the tik file found in the
 # NSP bundle, and extracts it down to the required ROMFS
 
+md .\work
+
 .\3rdparty\hactool.exe -x $1 --pfs0dir=.\work -t pfs0
 .\3rdparty\hactool.exe -x .\work\97105c79474118d294c8808a22f39bd3.nca `
-    --titlekey=$(Format-Hex -Count 16 -Offset 0x180 -Path .\work\0100d12014fc2000000000000000000b.tik) `
+    --titlekey=$(Format-Hex -Path .\work\0100d12014fc2000000000000000000b.tik -Count 16 -Offset 0x180) `
     --romfsdir=.\work
 
 Remove-Item .\work\*.nca .\work\*.tik .\work\*.cert
