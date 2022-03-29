3bf1b4210364a77d95d68aa81e6db6701933b152
TheSola10 <me@thesola.io>
2022-03-29 22:30:59 +0200

    Bonus round: GNU parallel support!

--- repack_scrpt.cpk.sh
+++ repack_scrpt.cpk.sh
@@ -5,14 +5,21 @@ DIR=$(realpath $(dirname $0))
 mkdir -p $DIR/work/Data/StreamingAssets/scrpt.cpk.contents \
          $DIR/out/contents/0100D12014FC200/romfs/Data/StreamingAssets
 
-for file in $DIR/patches/Data/StreamingAssets/scrpt.cpk/*.json
-do
-  {
-    echo "Encoding $(basename $file)"
-    python3 $DIR/inucode.py $file $DIR/work/Data/StreamingAssets/scrpt.cpk.contents/$(basename $file | head -c-6)
-  } &
-done
-wait $(jobs -p) # Wait for all encoders to finish before bundling.
+if which -p parallel >/dev/null 2>&1
+then
+    for file in $DIR/patches/Data/StreamingAssets/scrpt.cpk/*.json
+    do echo $file
+    done | parallel --progress --bar python3 $DIR/inucode.py {} $DIR/work/Data/StreamingAssets/scrpt.cpk.contents/{/.}
+else
+    for file in $DIR/patches/Data/StreamingAssets/scrpt.cpk/*.json
+    do
+      {
+        echo "Encoding $(basename $file)"
+        python3 $DIR/inucode.py $file $DIR/work/Data/StreamingAssets/scrpt.cpk.contents/$(basename $file | head -c-6)
+      } &
+    done
+    wait $(jobs -p) # Wait for all encoders to finish before bundling.
+fi
 
 wine $DIR/3rdparty/cpkmakec.exe \
         Z:$DIR/work/Data/StreamingAssets/scrpt.cpk.contents \
