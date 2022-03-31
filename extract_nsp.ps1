012a1b999d8e470b1977d9a3963fa6744c9da120
TheSola10 <me@thesola.io>
2022-03-31 17:58:30 +0200

    Improved readaibility of powershell script

--- extract_nsp.ps1
+++ extract_nsp.ps1
@@ -4,9 +4,11 @@
 
 md .\work
 
+$TitleKey = (Get-Content .\work\0100d12014fc2000000000000000000b.tik -Encoding byte)[0x180..(0x180+15)]
+
 .\3rdparty\hactool.exe -x $args[0] --pfs0dir=.\work -t pfs0
-.\3rdparty\hactool.exe -x .\work\97105c79474118d294c8808a22f39bd3.nca `
-    --titlekey=$(((Get-Content .\work\0100d12014fc2000000000000000000b.tik -Encoding byte)[0x180..(0x180+15)] | ForEach-Object ToString x2) -join '' ) `
+.\3rdparty\hactool.exe -x .\work\97105c79474118d294c8808a22f39bd3.nca   `
+    --titlekey=$($TitleKey | ForEach-Object ToString x2) -join '' )     `
     --romfsdir=.\work
 
 Remove-Item .\work\*.nca
