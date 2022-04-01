70b35b338c4580850b398a4311eaa88c97271b16
TheSola10 <me@thesola.io>
2022-04-01 22:02:27 +0200

    Fixed order of hactool calls

--- extract_nsp.ps1
+++ extract_nsp.ps1
@@ -4,9 +4,10 @@
 
 md .\work
 
+.\3rdparty\hactool.exe -x $args[0] --pfs0dir=.\work -t pfs0
+
 $TitleKey = (Get-Content .\work\0100d12014fc2000000000000000000b.tik -Encoding byte)[0x180..(0x180+15)]
 
-.\3rdparty\hactool.exe -x $args[0] --pfs0dir=.\work -t pfs0
 .\3rdparty\hactool.exe -x .\work\97105c79474118d294c8808a22f39bd3.nca   `
     --titlekey=$(($TitleKey | ForEach-Object ToString x2) -join '' )    `
     --romfsdir=.\work
